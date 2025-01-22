f84938e94d5b0dc09fe19b8c601392fc28ecba0a
lazy34 <maitysurajit0901@gmail.com>
2025-01-22 18:53:19 +0530

    add Progress component and enhance HistoryCard with edit/delete buttons and participation accuracy display

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { HistoryCard } from '@/components/custom/HistoryCard';
 import React from 'react'
 import { quizData } from '@/lib/content';
+import { Progress } from '@/components/ui/progress';
 
 const page = () => {
   return (
@@ -12,15 +13,20 @@ const page = () => {
                         Hello Surajit👋🏻
                     </h3>
                 </div>
-                <div className=' row-span-2 col-span-2 text-2xl font-bold '>
+                <div className=' row-span-2 col-span-2 p-2 overflow-y-scroll'>
+                    <h2 className='text-2xl font-bold '>History</h2>
                     {
                         quizData.map((data,key)=>(
                             <HistoryCard key={key} date={data.date} about={data.about} title={data.title} visibility={data.visibility}/>
                         ))
                     }
                 </div>
-                <div className=' bg-blue-300 row-start-1 row-span-3 col-start-3 text-2xl font-bold '>
-                    Participation Section
+                <div className=' row-start-1 row-span-3 col-start-3 text-2xl font-bold flex flex-col'>
+                    <h2 className='text-2xl font-bold'>Participation</h2>
+                    <h3 className='text-base font-bold'>Accuracy</h3>
+                    <span className='text-3xl font-thin'>63.4</span>
+                    <Progress value={63.4}/>
+                    
                 </div>
             </div>
         </main>
--- src/components/custom/HistoryCard.tsx
+++ src/components/custom/HistoryCard.tsx
@@ -1,11 +1,25 @@
 import { HistoryBar } from '@/lib/types'
 import React from 'react'
 import { Card } from '../ui/card'
+import { Button } from '../ui/button'
 
 export const HistoryCard = (params:HistoryBar) => {
   return (
     <>
-        <Card/>
+        <Card className='flex flex-col m-4 p-4'>
+          {params.date}
+          <div className='flex flex-row justify-between'>
+            {params.title}
+            <div>
+              <Button>Edit</Button>
+              <Button>Delete</Button>
+            </div>
+          </div>
+          <div className='flex flex-row justify-between'>
+            {params.about}
+            <span className='p-3 bg-black text-white rounded-[24px] text-base'>{params.visibility}</span>
+          </div>
+        </Card>
     </>
   )
 }
