5a0308df80b095dbb8623389fc6cd63b0c6005be
lazy34 <maitysurajit0901@gmail.com>
2025-01-22 17:11:37 +0530

    refactor navigation and authentication flow; update Nav component to toggle between login/logout and modify navigation elements

--- src/components/custom/Nav.tsx
+++ src/components/custom/Nav.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {
   Menubar,
   MenubarMenu,
@@ -7,8 +8,16 @@ import React from "react";
 import { Button } from "../ui/button";
 import Image from "next/image";
 import { NavElement } from "@/lib/content";
+import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 function Nav() {
+
+  const {user} = useUser();
+  console.log(user);
+  
+  
+
   return (
     <>
       <div className="h-12 w-full flex justify-between items-center  pl-6 pr-6 md:pl-12 md:pr-12 pt-4 pb-2 absolute top-0 left-0">
@@ -19,13 +28,16 @@ function Nav() {
         <Menubar className=" border-none outline-none flex-center">
           {NavElement.map((element, key) => (
             <MenubarMenu key={key}>
-              <MenubarTrigger className="font-[Inter] hidden font-normal tracking-wide xl:block">{element}</MenubarTrigger>
+              <MenubarTrigger className={`font-[Inter] hidden font-normal tracking-wide xl:block ${key===3?'bg-gradient-to-r from-primary to-accent text-white ':''}`}>{element}</MenubarTrigger>
             </MenubarMenu>
           ))}
         </Menubar>
-        <div>
-          <Button className="rounded-[24px]">Login</Button>
-        </div>
+        {user?(<div>
+          <Link href={'/api/auth/logout'}><Button className="rounded-[24px]">Logout</Button></Link>
+        </div>):(<div>
+          <Link href={'/api/auth/login'}><Button className="rounded-[24px]">Login</Button></Link>
+        </div>)}
+        
       </div>
     </>
   );
--- src/lib/content.ts
+++ src/lib/content.ts
@@ -1,3 +1,3 @@
 export const heroline = "Ready, Set, Quiz! Discover Your Knowledge Today!";
 export const subHeroLine = "Join a community of quiz lovers and challenge yourself with a variety of topics! Test your knowledge, compete with friends, and discover new facts - all while having fun. Let the quiz adventure begin!";
-export const NavElement  = ["History","Participate","Leaderboard","About"];
+export const NavElement  = ["Dashboard","Public Quiz","About","Create with AI"];
