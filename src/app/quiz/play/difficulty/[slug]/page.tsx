d909b18a1db788692c3d108fecf8210349b62fd2
lazy34 <maitysurajit0901@gmail.com>
2025-03-04 18:44:23 +0530

    feat: fetch public quizzes and update state in DifficultyQuizzesPage component

--- src/app/quiz/play/difficulty/[slug]/page.tsx
+++ src/app/quiz/play/difficulty/[slug]/page.tsx
@@ -6,13 +6,30 @@ import { useAtom } from "jotai";
 import { useParams } from "next/navigation";
 import { QuizDetails } from "@/lib/types";
 import { FetchedPublicQuizDetails } from "@/app/store/atom";
+import { getPublicQuizzes } from "@/app/db/queries/select";
 import QuizCard from "@/components/custom/QuizCard";
 
 const DifficultyQuizzesPage = () => {
   const router = useRouter();
-    const params = useParams<{ slug: string }>();
-  const [publicQuizDetails] = useAtom<QuizDetails[] | undefined>(FetchedPublicQuizDetails);
+  const params = useParams<{ slug: string }>();
+  const [publicQuizDetails, setPublicQuizDetails] = useAtom<QuizDetails[] | undefined>(FetchedPublicQuizDetails);
   const [filteredQuizzes, setFilteredQuizzes] = useState<QuizDetails[]>([]);
+
+  useEffect(() => {
+    const fetchQuizzes = async () => {
+      if (!publicQuizDetails) {
+        try {
+          const quizzes = await getPublicQuizzes();
+          if (quizzes.success) {
+            setPublicQuizDetails(quizzes.data);
+          }
+        } catch (error) {
+          console.error("Error fetching quizzes:", error);
+        }
+      }
+    };
+    fetchQuizzes();
+  }, [setPublicQuizDetails, publicQuizDetails]);
 
   useEffect(() => {
     if (publicQuizDetails) {
@@ -25,13 +42,13 @@ const DifficultyQuizzesPage = () => {
   };
 
   return (
-    <main className="flex flex-col justify-center px-4 h-auto">
+    <main className="flex flex-col justify-center items-center px-4 h-auto">
       <h1 className="text-3xl font-bold md:text-4xl mt-16 capitalize">{params.slug} Quizzes</h1>
       <p className="text-base xl:text-2xl mb-8 xl:mt-2 text-center">
         Browse through quizzes categorized under {params.slug} difficulty.
       </p>
       {filteredQuizzes.length > 0 ? (
-        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+        <div className="w-full flex flex-wrap justify-center gap-4">
           {filteredQuizzes.map((quiz) => (
             <div key={quiz.id} onClick={() => handleQuizClick(quiz.id)} className="cursor-pointer">
               <QuizCard quiz={quiz} />
