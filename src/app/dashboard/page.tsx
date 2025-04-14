ff98f158679f9be9d125c8c2d507c8c0b8fd3d4f
lazy34 <maitysurajit0901@gmail.com>
2025-04-14 16:22:00 +0530

    make dashboard responsive for devices

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -61,8 +61,8 @@ const page = () => {
         </main>
       ) : (
         <main>
-          <div className="grid h-auto xl:h-screen w-full grid-cols-1 xl:grid-cols-2 grid-rows-6 xl:grid-rows-[auto_auto_1fr] pt-20 pb-4 gap-2">
-            <div className=" col-span-2 xl:col-span-1 row-span-2 md:grid-rows-1 sm:row-span-1">
+          <div className="grid h-screen xl:h-screen w-full grid-cols-1 xl:grid-cols-2 grid-rows-4 xl:grid-rows-3 pt-20 pb-4 gap-2">
+            <div className="col-span-1 row-start-1 row-span-2 xl:row-span-1 md:row-span-1 sm:row-span-1">
               <h3 className="text-3xl font-bold">Hello {user?.nickname}👋🏻</h3>
               <div className="flex flex-row gap-4 flex-wrap p-4 items-center justify-center sm:h-full">
                 <Link href="/quiz/create" className="flex-1">
@@ -132,16 +132,16 @@ const page = () => {
                 </Link>
               </div>
             </div>
-            <div className="col-span-1 row-span-1 xl:col-span-1 p-2 mt-2 h-full w-full">
+            <div className="col-span-1 row-start-3 xl:row-start-2 row-span-1 xl:col-span-1 p-2 mt-2 h-full w-full place-self-center">
               <h2 className="text-2xl font-bold sticky block">
                 Trending topics
               </h2>
-              <div className="m-2 w-full flex items-start justify-center flex-col">
+              <div className="m-2 mt-8 w-full flex items-start justify-center flex-col">
                 <InfiniteScroll type="left" />
                 <InfiniteScroll type="right" />
               </div>
             </div>
-            <div className="row-span-2 col-span-2 xl:col-span-1 p-2 mt-2 h-full w-full relative overflow-hidden">
+            <div className="row-start-4 xl:row-start-3 row-span-1 col-span-2 xl:col-span-1 p-2 mt-2 h-full w-full relative overflow-hidden">
               <h2 className="text-2xl font-bold sticky block">
                 Stat of last participation
               </h2>
