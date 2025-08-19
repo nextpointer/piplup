f7c632e450d4b84c6117d542a480f5b1396a509e
nextpointer <110530249+nextpointer@users.noreply.github.com>
2025-08-20 00:14:14 +0530

    fix: fix sonner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono,Inter } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/custom/Nav";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { Provider } from "jotai";
 import { Toaster } from "@/components/ui/sonner";
 
-
 const InterFont = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
 });
-
 
 export const metadata: Metadata = {
   title: "Piplup",
@@ -27,14 +25,12 @@ export default function RootLayout({
     <html lang="en">
       <UserProvider>
         <Provider>
-      <body
-        className={`${InterFont.className}  antialiased `}
-      >
-        <Nav/>
-        {children}
-        <Toaster/>
-      </body>
-      </Provider>
+          <body className={`${InterFont.className}  antialiased `}>
+            <Nav />
+            {children}
+            <Toaster position="top-right" />
+          </body>
+        </Provider>
       </UserProvider>
     </html>
   );
